010ee753ce3d3a79075f7dbe9e31ba7bde5c8909
David Bowers <82392777+dnlbowers@users.noreply.github.com>
2023-03-22 23:13:19 +0100

    add mat tbale sorting classes and logic

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -26,6 +26,9 @@ export class CourseComponent implements OnInit, AfterViewInit {
     @ViewChild(MatPaginator)
     paginator: MatPaginator;
 
+    @ViewChild(MatSort)
+    sort: MatSort;
+
     displayedColumns = ['seqNo', "description", "duration"];
 
     constructor(private route: ActivatedRoute,
@@ -44,7 +47,12 @@ export class CourseComponent implements OnInit, AfterViewInit {
 
       this.loading = true;
 
-      this.coursesService.findLessons(this.course.id, "asc", this.paginator?.pageIndex ?? 0, this.paginator?.pageSize ?? 3)
+      this.coursesService.findLessons(
+        this.course.id, 
+        this.sort?.direction ?? "asc", 
+        this.paginator?.pageIndex ?? 0, 
+        this.paginator?.pageSize ?? 3,
+        this.sort?.active ?? "seqNo")
         .pipe(
           tap(lessons => this.lessons = lessons),
           catchError(err => {
@@ -60,7 +68,9 @@ export class CourseComponent implements OnInit, AfterViewInit {
 
     ngAfterViewInit() {
 
-      this.paginator.page
+      this.sort.sortChange.subscribe(() => this.paginator.pageIndex = 0);
+      
+      merge(this.sort.sortChange, this.paginator.page)
       .pipe(
         tap(() => this.loadLessonsPage())
       ).subscribe();
