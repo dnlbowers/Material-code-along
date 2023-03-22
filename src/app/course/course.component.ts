11b9c43b4e3cfdb5c521f4b815db91277066450a
David Bowers <dbowers@greentube.com>
2023-03-22 17:50:54 +0100

    add mmaterial loading spinner

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -5,7 +5,7 @@ import { MatSort } from "@angular/material/sort";
 import { MatTableDataSource } from "@angular/material/table";
 import {Course} from "../model/course";
 import {CoursesService} from "../services/courses.service";
-import {debounceTime, distinctUntilChanged, startWith, tap, delay, catchError} from 'rxjs/operators';
+import {debounceTime, distinctUntilChanged, startWith, tap, delay, catchError, finalize} from 'rxjs/operators';
 import {merge, fromEvent, throwError, pipe} from "rxjs";
 import { Lesson } from '../model/lesson';
 
@@ -20,6 +20,8 @@ export class CourseComponent implements OnInit, AfterViewInit {
     course:Course;
 
     lessons : Lesson[] = [];
+
+    loading: boolean = false;
 
     displayedColumns = ['seqNo', "description", "duration"];
 
@@ -36,6 +38,9 @@ export class CourseComponent implements OnInit, AfterViewInit {
     }
 
     loadLessonsPage() {
+
+      this.loading = true;
+
       this.coursesService.findLessons(this.course.id, "asc", 0, 3)
         .pipe(
           tap(lessons => this.lessons = lessons),
@@ -43,7 +48,8 @@ export class CourseComponent implements OnInit, AfterViewInit {
             console.log("Error loading lessons", err);
             alert("Error loading lessons")
             return throwError(err);
-          })
+          }),
+          finalize(() => this.loading = false)
         )
         .subscribe();
         
