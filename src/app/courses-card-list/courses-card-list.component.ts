739e30e2526d75857859a10afdba8cf80f2cf3b9
David Bowers <dbowers@greentube.com>
2023-03-27 15:09:36 +0200

    add breakpoint observable for different devices to alter the number of columns and row height

--- src/app/courses-card-list/courses-card-list.component.ts
+++ src/app/courses-card-list/courses-card-list.component.ts
@@ -1,6 +1,8 @@
 import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
 import {Course} from "../model/course";
 import { MatDialog, MatDialogConfig } from "@angular/material/dialog";
+import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
+
 import { openEditCourseDialogue } from '../course-dialog/course-dialog.component';
 import { filter } from 'rxjs/operators';
 
@@ -14,11 +16,35 @@ export class CoursesCardListComponent implements OnInit {
     @Input()
     courses: Course[];
 
-    constructor(private dialog: MatDialog) {
+    cols = 3;
+    rowHeight = '500px';
+
+    constructor(private dialog: MatDialog, private responsive: BreakpointObserver) {
     }
 
     ngOnInit() {
+        this.responsive.observe([
+            Breakpoints.TabletPortrait,
+            Breakpoints.TabletLandscape,
+            Breakpoints.HandsetPortrait,
+            Breakpoints.HandsetLandscape
+        ]).subscribe(result => {
 
+            this.cols = 3;
+            this.rowHeight = '500px';
+
+            const breakpoints = result.breakpoints;
+            if (breakpoints[Breakpoints.TabletPortrait]) {
+                this.cols = 1;
+            } else if (breakpoints[Breakpoints.TabletLandscape]) {
+                this.cols = 2;
+            } else if (breakpoints[Breakpoints.HandsetPortrait]) {
+                this.cols = 1;
+                this.rowHeight = '430px';
+            } else if (breakpoints[Breakpoints.HandsetLandscape]) {
+                this.cols = 1;
+            }
+        });
     }
 
     editCourse(course:Course) {
